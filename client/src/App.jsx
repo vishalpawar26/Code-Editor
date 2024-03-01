27aa319c4cc5fc2bbf3dd9f1485010d2f0ff07df
Vishal Pawar <97228250+vishalpawar26@users.noreply.github.com>
2024-03-01 14:53:53 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
     };
 
     axios
-      .post("http://localhost:8001/run", formData)
+      .post("https://code-editor-server-one.vercel.app//run", formData)
       .then((res) => {
         console.log(res);
         setOutputData(res.data);
