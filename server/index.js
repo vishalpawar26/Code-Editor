7f202a6e6e4d3d7066752ccb57b2cae40fb779da
Vishal Pawar <97228250+vishalpawar26@users.noreply.github.com>
2024-03-01 15:15:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,13 +6,7 @@ const request = require("request");
 const app = express();
 app.use(express());
 app.use(express.json());
-app.use(cors(
-  {
-    origin: ["https://code-editor-client.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
+app.use(cors(*));
 
 dotenv.config();
 
