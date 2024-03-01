dbfce2f10233c14aed199bf21822e714e52f9961
Vishal Pawar <97228250+vishalpawar26@users.noreply.github.com>
2024-03-01 23:05:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use("/", (req, res) => {
   res.send("Server is running...");
 });
 
-app.post("/", async (req, res) => {
+app.post("/run", async (req, res) => {
   const { script, stdin, language, versionIndex } = req.body;
 
   var program = {
